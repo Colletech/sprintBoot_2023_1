7c1f1c5e7072818e4c8887fae1b9be6684ab0b68
Andy Gomez <45979588+Koato@users.noreply.github.com>
2023-08-12 09:00:43 -0500

    eureka-server

--- iglesia/src/main/java/com/colletech/iglesia/IglesiaApplication.java
+++ iglesia/src/main/java/com/colletech/iglesia/IglesiaApplication.java
@@ -2,8 +2,10 @@ package com.colletech.iglesia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class IglesiaApplication {
 
 	public static void main(String[] args) {
--- municipality/src/main/java/com/colletech/municipality/MunicipalityApplication.java
+++ municipality/src/main/java/com/colletech/municipality/MunicipalityApplication.java
@@ -2,8 +2,10 @@ package com.colletech.municipality;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MunicipalityApplication {
 
 	public static void main(String[] args) {
